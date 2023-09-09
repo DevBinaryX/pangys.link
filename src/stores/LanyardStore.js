6104fd111928f20109f3a48e15a4c059098ee5ae
DevBinaryX <136874079+DevBinaryX@users.noreply.github.com>
2023-09-09 18:14:10 +0800

    Update LanyardStore.js

--- src/stores/LanyardStore.js
+++ src/stores/LanyardStore.js
@@ -1,7 +1,7 @@
 import { readable } from 'svelte/store';
 
 const fetchLanyard = () => {
-	const json = fetch('https://api.lanyard.rest/v1/users/399862294143696897').then((r) => r.json());
+	const json = fetch('https://api.lanyard.rest/v1/users/869501647548743720').then((r) => r.json());
 	console.log(json);
 	return json;
 };
